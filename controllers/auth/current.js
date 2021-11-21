b3029318c47381d6dfb56b77d6afc686e0d06ca7
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-11-21 21:03:28 +0200

    fix

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -3,6 +3,8 @@ const { User } = require('../../models')
 const sendSuccessResponse = require('../../helpers/sendSuccessResponse')
 
 const current = async (req, res) => {
+  // const { _id } = req.user
+  // const data = await User.findById(_id, 'email balance token')
   const { email } = req.user
   const data = await User.findOne({ email })
   console.log('dataCurrent', data)
--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,7 +1,10 @@
 const { User } = require('../../models')
+// const sendSuccessResponse = require('../../helpers')
 
 const logout = async (req, res) => {
   console.log('logout')
+  // const { _id } = req.user
+  // await User.findByIdAndUpdate(_id, { token: null })
   const { email } = req.user
   await User.findOneAndUpdate({ email }, { token: null })
   res.json({
