c2f0ff530cd74fb6d37fee450541e3d0c3a77ba1
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-11-22 15:43:45 +0200

    clean code

--- models/index.js
+++ models/index.js
@@ -1,9 +1,7 @@
 const { User } = require('./user')
-const { Session } = require('./session')
 const { Transaction } = require('./transaction')
 
 module.exports = {
   User,
-  Session,
   Transaction
 }
